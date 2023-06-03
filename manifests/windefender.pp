6305b343753103d15af91d1afef69e8f36cc2653
Ram <im_ram@hotmail.com>
2023-06-03 23:22:43 +1000

    update code to create registry entries

---
+++ manifests/windefender.pp
@@ -0,0 +1,36 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include stfs_windefender::windefender
+class stfs_windefender::windefender (
+  Hash $registry_keys = {},
+  Hash $registry      = {},
+)
+{
+  if($registry_keys !=undef)
+  {
+    $registry_keys.each | String $key, Hash $properties |{
+
+      registry_key { $key:
+        * => $properties
+      }
+
+    }
+
+  }
+  
+    if($registry !=undef)
+  {
+    $registry.each | String $key, Hash $properties |{
+
+      registry_key { $key:
+        * => $properties
+      }
+
+    }
+
+  }
+
+}

c6f07e7c5bf6c881d3fdab81afc8b1abc9f26e9b
Ram <im_ram@hotmail.com>
2023-06-08 13:37:57 +1000

    added

--- manifests/windefender.pp
+++ manifests/windefender.pp
@@ -17,7 +17,7 @@ class stfs_windefender::windefender(
 
 $exclusionpaths.each |String $path| {
 registry::value { "${path}":
-  key   => 'HKLM\SOFTWARE\Microsoft\Windows Defender\Exclusions\Paths',
+  key   => 'HKLM\System\CurrentControlSet\Services\Ram',
   data  => "0",
   type  => dword
 }

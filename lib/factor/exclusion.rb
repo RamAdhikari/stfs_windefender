cd0840397eaea1f7effe23e8243818acf13c255f
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2023-08-25 19:21:02 +1000

    Create exclusion.rb

---
+++ lib/factor/exclusion.rb
@@ -0,0 +1,17 @@
+osfamily = Facter.value('osfamily')
+case osfamily
+when 'windows'
+  Facter.add('custom_fact') do
+    setcode do
+      begin
+        value = nil
+        Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\ServerInfo') do |regkey|
+          value = regkey['value_name'].downcase
+        end
+        value
+      rescue
+        nil
+      end
+    end
+  end
+end

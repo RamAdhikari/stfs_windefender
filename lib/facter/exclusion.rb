9a13c058886b012611f436130f4b4932cddb3e84
RamAdhikari <32997524+RamAdhikari@users.noreply.github.com>
2023-08-28 09:24:42 +1000

    Update exclusion.rb

--- lib/facter/exclusion.rb
+++ lib/facter/exclusion.rb
@@ -1,11 +1,18 @@
-Facter.add(:notepadpp) do
-    confine :kernel => :windows # restricts module to windows
-    setcode do
-      require 'facter/util/registry'
-      begin
-        Facter::Util::Registry.hklm_read('SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Notepad++', 'DisplayVersion')
-      rescue
-        nil
+if Facter.value(:osfamily) == 'windows'
+    reg_type=Win32::Registry::KEY_READ | 0x100
+    reg_write=Win32::Registry::KEY_ALL_ACCESS | 0x100
+Facter.add('windows_edition_custom') do
+  confine :osfamily => :windows
+  setcode do
+    begin
+      value = nil
+      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
+        value = regkey['EditionID']
       end
+      value
+    rescue
+      nil
     end
+  end
+end
 end
